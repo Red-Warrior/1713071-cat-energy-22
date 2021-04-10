9f3e7063ae109b18c79a6d6c35dc4f3c399fef14
Red-Warrior <sword_and_brush@mail.ru>
2021-04-10 21:45:56 +0300

    form page style

--- source/main.js
+++ source/main.js
@@ -3,20 +3,44 @@ let menuMain = document.querySelector(".main-nav");
 let menuButton = document.querySelector(".page-header__toggle");
 let menuImage = menuButton.querySelector("img");
 
+function beforeUploading() {
+  menuMain.classList.remove("main-nav--show-menu");
+  setTimeout(() => {
+    menuMain.setAttribute("style", "transition: max-height 0.5s ease-in-out;");
+  });
+}
+
+function baseButton() {
+  menuImage.setAttribute("src", "img/base-button.svg");
+  menuImage.setAttribute("width", "24");
+  menuImage.setAttribute("height", "18");
+}
+
+function closeButton() {
+  menuImage.setAttribute("src", "img/close.svg");
+  menuImage.setAttribute("width", "20");
+  menuImage.setAttribute("height", "20");
+}
+
+function toggleButton() {
+  menuMain.classList.toggle("main-nav--show-menu");
+  toggle = !toggle;
+}
+
+document.addEventListener("DOMContentLoaded", beforeUploading);
 menuButton.addEventListener("click", openMenu);
+
+window.onunload = () => {
+  document.removeEventListener("DOMContentLoaded", beforeUploading);
+  menuButton.removeEventListener("click", openMenu);
+};
 
 function openMenu() {
   if (toggle) {
-    menuImage.setAttribute("src", "img/close.svg");
-    menuImage.setAttribute("width", "20");
-    menuImage.setAttribute("height", "20");
-    menuMain.classList.toggle("main-nav--show-menu")
-    toggle = !toggle;
+    closeButton();
+    toggleButton();
   } else {
-    menuImage.setAttribute("src", "img/base-button.svg");
-    menuImage.setAttribute("width", "24");
-    menuImage.setAttribute("height", "18");
-    menuMain.classList.toggle("main-nav--show-menu")
-    toggle = !toggle;
+    baseButton();
+    toggleButton();
   }
 }
